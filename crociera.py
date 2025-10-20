15889e7cd3de55a4da7db0f340a82fcfa78c27a7
elenacasto <elenacasto06@gmail.com>
2025-10-20 22:27:56 +0200

    completo

--- crociera.py
+++ crociera.py
@@ -17,8 +17,8 @@ class Crociera:
     def nome(self):
         return self._nome
     @nome.setter
-    def nome(self, valore):
-        self._nome = valore
+    def nome(self, nome):
+        self._nome = nome
 
     def carica_file_dati(self, file_path):
         """Carica i dati (cabine e passeggeri) dal file"""
@@ -26,23 +26,23 @@ class Crociera:
         with open(file_path, "r", encoding="utf-8") as file:
             for line in file:
                 line = line.strip().split(",")
+
                 if line[0].startswith("CAB"):
-                    codice_cabina = line[0]
+                    codice = line[0]
                     num_letti = int(line[1])
                     ponte = int(line[2])
                     prezzo = float(line[3])
 
                     if len(line) == 4:
-                        cabina = CabinaStandard(codice_cabina, num_letti, ponte, prezzo)
+                        cabina = CabinaStandard(codice, num_letti, ponte, prezzo)
                     elif line[4].isdigit():
                         max_animali = int(line[4])
-                        cabina = CabinaAnimali(codice_cabina, num_letti, ponte, prezzo, max_animali)
+                        cabina = CabinaAnimali(codice, num_letti, ponte, prezzo, max_animali)
                     else :
                         stile_deluxe = line[4]
-                        cabina = CabinaDeluxe(codice_cabina, num_letti, ponte, prezzo, stile_deluxe)
+                        cabina = CabinaDeluxe(codice, num_letti, ponte, prezzo, stile_deluxe)
 
                     self._cabina.append(cabina)
-
 
                 elif line[0].startswith("P"):
                     codice_passeggero = line[0]
@@ -55,7 +55,7 @@ class Crociera:
     def assegna_passeggero_a_cabina(self, codice_cabina, codice_passeggero):
         """Associa una cabina a un passeggero"""
         cabina = next((c for c in self._cabina if c.codice == codice_cabina), None)
-        passeggero = next((p for p in self._passeggeri if p.codice == codice_cabina), None)
+        passeggero = next((p for p in self._passeggeri if p.codice_passeggero == codice_passeggero), None)
 
         if cabina is None:
             print("Cabina non esistente")
@@ -68,6 +68,7 @@ class Crociera:
             return
         if passeggero.cabina is not None:
             print("Passeggero già assegnato a una cabina")
+            return
 
         cabina.assegna_passeggero(passeggero)
         passeggero.cabina = cabina
@@ -75,9 +76,10 @@ class Crociera:
 
     def cabine_ordinate_per_prezzo(self):
         """Restituisce la lista ordinata delle cabine in base al prezzo"""
-        return sorted(self._cabina)
+        return sorted(self._cabina, key=lambda c: c.prezzo)
 
     def elenca_passeggeri(self):
         """Stampa l'elenco dei passeggeri mostrando, per ognuno, la cabina a cui è associato, quando applicabile """
         for passeggero in self._passeggeri:
+            cabina_info = f"Cabina: {passeggero.cabina.codice}" if passeggero.cabina else "Nessuna cabina assegnata"
             print(passeggero)
